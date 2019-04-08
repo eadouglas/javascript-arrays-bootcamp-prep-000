7400f8ed81431144c37eb4142cfe076077814d53
eustace douglas <eadouglas@live.co.uk>
2019-04-08 22:45:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] ;
+ function addElementToBeginningOfArray(ar, el){
+ let chocolateBars2 = [(ar,el),learn...chocolateBars];
+}
+
+// function destructivelyAddElementToEndOfArray(ar,el){
+//   chocolateBars.push()
+// }
